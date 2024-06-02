f8704e1e16189a059e272488f40a8f0046541602
KisMett03 <kailiang0120@gmail.com>
2024-06-02 18:13:08 +0800

    admin page added

--- lib/adminpage/adminpage.dart
+++ lib/adminpage/adminpage.dart
@@ -1,38 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class AdminPage extends StatelessWidget {
+class AdminPage extends StatefulWidget {
+  @override
+  _AdminPageState createState() => _AdminPageState();
+}
+
+class _AdminPageState extends State<AdminPage> {
+  int _selectedIndex = 0;
+  bool _hasUnreadNotifications = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkForUnreadNotifications();
+  }
+
+  Future<void> _checkForUnreadNotifications() async {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+    QuerySnapshot snapshot = await firestore.collection('notifications')
+        .where('isRead', isEqualTo: false)
+        .get();
+
+    setState(() {
+      _hasUnreadNotifications = snapshot.docs.isNotEmpty;
+    });
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Admin Page'),
-      ),
-      body: Center(
-        child: Text('Welcome to the Admin Page!'),
+        actions: [
+          IconButton(
+            icon: Icon(
+              _hasUnreadNotifications ? Icons.notifications_active_rounded : Icons.notifications_rounded,
+              color: _hasUnreadNotifications ? Colors.orange : Colors.grey,
+            ),
+            onPressed: () {
+              // show notifications page
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => NotificationsPage()),
+              ).then((value) => _checkForUnreadNotifications()); // Recheck for unread notifications when returning
+            },
+          ),
+        ],
+        title: const Text('JomEat'),
+        leading: Image.asset('assets/images/logo.png'),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        items: [
+        type: BottomNavigationBarType.fixed,
+        items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
+            icon: Icon(Icons.home_rounded),
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
+            icon: Icon(Icons.settings_rounded),
             label: 'Settings',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
+            icon: Icon(Icons.bar_chart_rounded),
+            label: 'Reports',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_rounded),
             label: 'Profile',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.notifications),
-            label: 'Notifications',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.exit_to_app),
-            label: 'Logout',
-          ),
+          )
         ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.orange,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class NotificationsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notifications'),
+      ),
+      body: const Center(
+        child: Text('Here are your notifications.'),
       ),
     );
   }
